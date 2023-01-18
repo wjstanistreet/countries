704d10956b50e4a05cc67cd86609d47ee29d9c47
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2023-01-18 15:16:05 +0000

    Step 5: logging specific country

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("HI!");
+
+const countryPromises = [];
+const getCountryByName = async (countryName) => {
+    // Fetch array
+    const countryArray = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => {return response.json()})
+    
+    // Find the country based on search
+    let country;
+    for (let i = 0; i < countryArray.length; i++) {
+        if (countryArray[i].name.common.toLowerCase() === countryName.toLowerCase()) {
+            country = countryArray[i];
+        }
+    }
+    console.log(country);
+};
+
+getCountryByName("united");
